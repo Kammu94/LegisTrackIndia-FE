a068254c2c66b24f6041480e66830901a3f4ba36
Kamran Basit <kamranqdn@gmail.com>
2026-05-06 17:30:12 +0530

    produrl change

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -4,8 +4,8 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   tagTypes: ['Cases', 'Hearings'],
   baseQuery: fetchBaseQuery({ 
-    // baseUrl: 'https://legistrack-baemf0g6b0hvckfh.centralindia-01.azurewebsites.net/api', // Prod
-	baseUrl: 'https://localhost:7289/api',//local
+    baseUrl: 'https://legistrack-baemf0g6b0hvckfh.centralindia-01.azurewebsites.net/api', // Prod
+	//baseUrl: 'https://localhost:7289/api',//local
 	
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as { auth: { token: string | null } };
